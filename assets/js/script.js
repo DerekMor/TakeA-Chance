7f8895042c7559e171da51fbc726476c749704a1
DerekMor <derekmoriarty24@gmail.com>
2022-10-02 14:21:15 +0000

    Add js to show icons each side picks and result message

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,9 @@ document.getElementById('spock').onclick = play;
 function play(){
     let userGuess = this.id;
     let compGuess = getCompGuess();
-    console.log(userGuess);
-    console.log(compGuess);
+    
+    displayGuesses(userGuess, compGuess);
+    checkWinner(userGuess, compGuess);
 }
 
 
@@ -38,4 +39,57 @@ function getCompGuess(){
     }
 
     return compGuess;
+}
+
+/**
+ * Takes in the guesses for both user and computer then passes them into another function
+ * which gives a returned value for us to change the icons in each guess box
+ */
+function displayGuesses(guess1, guess2){
+    let userGuessIcon = findIcon(guess1);
+    let compGuessIcon = findIcon(guess2);
+
+    document.getElementById('your-choice').innerHTML = userGuessIcon;
+    document.getElementById('comp-choice').innerHTML = compGuessIcon;
+}
+
+/**
+ * Takes the id of the users and computers guesses and
+ * @returns the html code for the icons
+ */
+function findIcon(guess){
+    let iconHtml = '';
+    if (guess === 'rock'){
+        iconHtml = '<i class="fa-regular fa-hand-back-fist"></i>';
+    } else if (guess === 'paper'){
+        iconHtml = '<i class="fa-regular fa-hand"></i>';
+    } else if (guess === 'scissors'){
+        iconHtml = '<i class="fa-regular fa-hand-scissors"></i>';
+    } else if (guess === 'lizard') {
+        iconHtml = '<i class="fa-regular fa-hand-lizard"></i>';
+    } else if (guess === 'spock') {
+        iconHtml = '<i class="fa-regular fa-hand-spock"></i>';
+    }
+
+    return iconHtml;
+}
+
+function checkWinner(userGuess, compGuess){
+    let user = userGuess;
+    let comp = compGuess;
+
+    // result messages
+    let win = '<h3>You Win!</h3>';
+    let lose = '<h3>You Lose!</h3>';
+    let draw = '<h3>Draw!</h3>';
+
+    if(user === 'rock'){
+        if(comp === 'rock'){
+            document.getElementById('result').innerHTML = draw;
+        } else if (comp === 'scissors' || comp === 'lizard'){
+            document.getElementById('result').innerHTML = win;
+        } else {
+            document.getElementById('result').innerHTML = lose;
+        }
+    }
 }
